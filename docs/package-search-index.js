0f86729a24226a0daac06620b76da61f64676abd
Johnny RAMANANJATOVO <ramananmichel@gmail.com>
2024-06-10 23:19:39 +0200

    Modification de certains éléments et rajout du documentation

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.esiee.baba.controller"},{"l":"fr.esiee.baba.model"},{"l":"fr.esiee.baba.view"}];updateSearchResults();
