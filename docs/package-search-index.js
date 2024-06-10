d04016fc41377c560176f7cfab3b5179890e1677
Johnny RAMANANJATOVO <ramananmichel@gmail.com>
2024-06-10 22:00:54 +0200

    Adding big changes : - Using ParserLine - Build.xml - New Rules Jump

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.esiee.baba.controller"},{"l":"fr.esiee.baba.model"},{"l":"fr.esiee.baba.view"}];updateSearchResults();
